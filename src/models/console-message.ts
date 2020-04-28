b0f5c6d053a774839aeeaa798d7958e8714be1a1
Praney Behl <praneybehl@gmail.com>
2020-04-28 22:39:01 +1000

    chore(models): add console messages

---
+++ src/models/console-message.ts
@@ -0,0 +1,8 @@
+export enum ConsoleMessage {
+    TITLE = 'ZOMBIELAND',
+    BANNER = 'Zombie Apocalypse v4.5 - interactive command-line application for Ailo coding challenge',
+    ERROR = 'ERROR: ',
+    SUCCESS = 'SUCCESS: ',
+    INFO = 'INFO: ',
+    START_GENERATING = 'Starting Zombieland Apocalypse...',
+}
