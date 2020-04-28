7aeba9ed241aec6b97700494860ab56aa7c8cdd7
Praney Behl <praneybehl@gmail.com>
2020-04-28 22:39:11 +1000

    feat: add logger utilities

--- index.ts
+++
@@ -1,5 +0,0 @@
-export function index(): Promise<any> {
-    return new Promise<any>(function resolve() {});
-}
-
-index();
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { ZombieLand } from './zombieland';
+
+export function index(): Promise<any> {
+    return ZombieLand();
+}
+
+index();
