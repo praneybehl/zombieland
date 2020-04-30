e5f408e3a50b63c5512aa02acf1cf682beb1b033
Praney Behl <praneybehl@gmail.com>
2020-04-30 22:26:05 +1000

    chore: add question exports

---
+++ src/questions/index.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+export * from './input-type';
+export * from './input-file';
+export * from './position';
+export * from './world-dimension';
+export * from './add-more';
+export * from './zombie-moves';
